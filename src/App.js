f607e78f01520156fc1ac451b775ce5ab9b88d18
glmsr12 <gulumsercakmak@hotmail.com>
2022-11-29 10:32:21 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1> Techie Road</h1>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+  return <Footer>footer</Footer>;
+};
+
+export default Footer;
